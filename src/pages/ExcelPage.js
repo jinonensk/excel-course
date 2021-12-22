43c31703f963e299eee6a7e225a41f6b48e4c308
Alexander <45040179+jinonensk@users.noreply.github.com>
2021-12-22 15:03:18 +0700

    implement deps invers principle (#10)

--- src/pages/ExcelPage.js
+++ src/pages/ExcelPage.js
@@ -1,7 +1,7 @@
-import {Page} from '@core/Page'
-// import {$} from '@core/dom'
-import {debounce, storage} from '@core/utils'
+import {Page} from '@core/page/Page'
 import {createStore} from '@core/store/createStore'
+import {StateProcessor} from '@core/page/StateProcessor'
+import {LocalStorageClient} from '../shared/LocalStorageClient'
 
 import {Excel} from '@/components/excel/Excel'
 import {Formula} from '@/components/formula/Formula'
@@ -11,22 +11,19 @@ import {Toolbar} from '@/components/toolbar/Toolbar'
 
 import {normalizeInitialState, rootReducer} from '@/redux'
 
-function storageName(param) {
-  return `excel:${param}`
-}
-
 export class ExcelPage extends Page {
-  getRoot() {
-    const params = this.params ? this.params : Date.now().toString()
+  constructor(param) {
+    super(param)
 
-    const state = storage(storageName(params))
+    this.storeSub = null
+    this.processor = new StateProcessor(new LocalStorageClient(this.params))
+  }
+
+  async getRoot() {
+    const state = await this.processor.get()
     const store = createStore(rootReducer, normalizeInitialState(state))
 
-    const stateListener = debounce(state => {
-      storage(storageName(params), state)
-    }, 300)
-
-    store.subscribe(stateListener)
+    this.storeSub = store.subscribe(this.processor.listen)
 
     this.excel = new Excel({
       components: [Header, Toolbar, Formula, Table],
@@ -42,5 +39,6 @@ export class ExcelPage extends Page {
 
   destroy() {
     this.excel.destroy()
+    this.storeSub.unsubscribe()
   }
 }
